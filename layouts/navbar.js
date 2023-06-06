15f3eb13f54af6ab9448657a77e305e8b6e965b0
elsa.m <manangelsa07@gmail.com>
2023-06-06 15:39:18 +0200

    background review

--- layouts/navbar.js
+++ layouts/navbar.js
@@ -1,24 +1,18 @@
 import {LeftC, RightC, User, DownOne} from '@icon-park/react';
 export default function Navbar(props) {
     return(
-        <div id='mynav' className={'grid grid-cols-5 mt-[-5px] text-white pt-2 pb-2 px-6' + props.color}>
+        <div id='mynav' className={'flex justify-between text-white pt-2 pb-2 px-6 ' + props.color}>
             <div className="col-span-4 flex">
                 <LeftC size='2.5em' className="mx-1" fill={'#000000'} theme="filled"/>
                 <RightC size='2.5em' className="mx-1" fill={'#000000'} theme="filled" />
                 {props.element}
             </div>
-            <div className="flex justify-end">
-                <div className="ml-4">
+            <div className="ml-40 flex justify-end">
+                <div className="m-2">
                     {props.sub}
                 </div>
-                <div className="ml-8">
-                    <button className="p-0 w-[160px] rounded-full border border-2 border-[#000000] font-bold bg-[#000000] flex">
-                        <p className="rounded-full w-[30px] bg-[#878787] p-1 mr-1"><User size='1em'/></p>
-                        <p className="m-1 mb-0">elsa manang</p>
-                        <p className="m-1 mb-0">
-                            <DownOne size="1.2em" className="mt-[3px]" fill={'#ffffff'} theme="filled"/>
-                        </p>
-                    </button>
+                <div className="m-2">
+                    <p className="p-2 mx-2 rounded-full font-bold bg-[#000000]"><User size='1.4em'/></p>
                 </div>
             </div>
         </div>
