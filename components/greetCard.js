e3173f7efc860dce1d863cf6bc932335f8443ecd
elsa.m <manangelsa07@gmail.com>
2023-06-06 20:19:56 +0200

    add play button

--- components/greetCard.js
+++ components/greetCard.js
@@ -1,11 +1,17 @@
 import Image from 'next/image'
+import {PlayOne} from "@icon-park/react";
 
 export default function GreetCard(props) {
 
     return(
-        <div className='flex m-3 w-[300px] h-[80px] rounded bg-[#353535]'>
-            <Image className='w-[80px] h-[80px] rounded' src={props.img} alt="" height={80} width={80} />
-            <p className='mx-4 mt-[30px] text-[16px] font-semibold'>{props.titre}</p>
+        <div className='group flex justify-between m-3 w-[300px] h-[100px] rounded bg-[#353535]/50 hover:bg-[#3a3a3a]'>
+            <div className={'flex'}>
+                <Image className='w-[100px] h-[100px] rounded' src={props.img} alt="" height={80} width={80} />
+                <p className='mx-4 mt-[30px] text-[16px] font-semibold'>{props.titre}</p>
+            </div>
+            <span className='relative invisible group-hover:visible shadow-lg mt-[28px] mr-[15px] pt-[10px] w-[50px] h-[50px] hover:w-[52px] hover:h-[52px] bg-[#1ed760] text-center rounded rounded-full'>
+                <PlayOne theme={'filled'} fill={'#000000'} size={28} />
+            </span>
         </div>
     )
 }
--- layouts/navbar.js
+++ layouts/navbar.js
@@ -1,5 +1,13 @@
+import React, {useState} from "react";
 import {LeftC, RightC, User, DownOne} from '@icon-park/react';
+import {Classes, Popover, Position} from "@blueprintjs/core";
 export default function Navbar(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const handleSetOpen = () => {
+        setIsOpen(!isOpen);
+    }
+    
     return(
         <div id='mynav' className={'flex justify-between text-white pt-2 pb-2 px-6 ' + props.color}>
             <div className="col-span-4 flex">
@@ -11,9 +19,18 @@ export default function Navbar(props) {
                 <div className="m-2">
                     {props.sub}
                 </div>
-                <div className="m-2">
-                    <p className="p-2 mx-2 rounded-full font-bold bg-[#000000]"><User size='1.4em'/></p>
-                </div>
+                <Popover position={Position.BOTTOM_LEFT} canEscapeKeyClose={false} minimal={true} className={'bg-[#191919]'}
+                    focus={false} isOpen={isOpen} enforceFocus={false} popoverClassName={Classes.POPOVER_CONTENT_SIZING}
+                    content={
+                        <div className='p-0 grid w-[300px] bg-[#191919] text-white'>
+                            <p>hgf</p>
+                        </div>
+                    } 
+                >
+                    <div className="m-0" onClick={handleSetOpen}>
+                        <p className="p-2 mx-2 rounded-full font-bold bg-[#000000]"><User size='1.4em'/></p>
+                    </div>
+                </Popover>
             </div>
         </div>
     )
