dcd0b303019655fd77ebeb499c8315014846eaf9
elsa.m <manangelsa07@gmail.com>
2023-06-12 18:05:35 +0200

    Play track and volume track

--- layouts/footer.js
+++ layouts/footer.js
@@ -4,9 +4,77 @@ import {
     ListTop, Microphone, PlayCycle, Shuffle,
     Sound, VolumeNotice
 } from "@icon-park/react";
+import {useEffect, useState} from "react";
 
 export default function Footer(props) {
+    
+    const [soundX, setSoundX] = useState(0);
+    const [volumeX, setVolumeX] = useState(0);
+    const [isVolumeChange, setIsVolumeChange] = useState(false);
+    const [gradient, setGradient] = useState(0);
+    const [color, setColor] = useState("#ffffff");
+    const [playX, setPlayX] = useState(0);
+    const [isPlayChange, setIsPlayChange] = useState(false);
+    const [gradientPlay, setGradientPlay] = useState(0);
+    const [colorPlay, setColorPlay] = useState("#ffffff");
 
+    useEffect(() => {
+        let volume = document.getElementById('volume');
+        let play = document.getElementById('play');
+        setSoundX(volume.getBoundingClientRect().left);
+        setPlayX(play.getBoundingClientRect().left);
+        let element = document.getElementById('myVolume');
+        setVolumeX(element.getBoundingClientRect().left);
+        //handleTrackStyle(volume.getBoundingClientRect().left-element.getBoundingClientRect().left-6);
+    }, [])
+    
+    const handleVolumeMove = () => {
+        setColor("#1ed760");
+        if(isVolumeChange) {
+            let element = document.getElementById('myVolume');
+            let e = window.event;
+            let diff = parseInt(e.clientX) - parseInt(volumeX);
+            if (diff > -1 && diff < 62) {
+                element.style.position = "relative"
+                element.style.left = diff+"px";
+                handleTrackStyle(diff);
+            }
+        }
+    }
+
+    const handlePlayMove = () => {
+        setColorPlay("#1ed760");
+        if(isPlayChange) {
+            let element = document.getElementById('myPlay');
+            let e = window.event;
+            let diff = parseInt(e.clientX) - parseInt(playX);
+            if (diff > -1 && diff < 318) {
+                element.style.position = "relative"
+                element.style.left = diff+"px";
+                handleTrackPlayStyle(diff);
+            }
+        }
+    }
+    
+    const handleTrackStyle = (button) => {
+        let pourcent = parseInt((button*100)/61);
+        setGradient(pourcent);
+    }
+
+    const handleTrackPlayStyle = (button) => {
+        let pourcent = parseInt((button*100)/318);
+        setGradientPlay(pourcent);
+    }
+    
+    const handleVolumeUp = (value) => {
+        setIsVolumeChange(value);
+        //console.log(value)
+    }
+
+    const handlePlayUp = (value) => {
+        setIsPlayChange(value);
+    }
+    
     return(
         <div className="flex h-[10vh] w-full text-white bg-[#000000]">
             <div className='flex my-1 mx-4 w-[450px]'>
@@ -30,7 +98,10 @@ export default function Footer(props) {
                 </div>
                 <div className="flex justify-center" style={{fontSize: '12px', color: "#b3b3b3"}}>
                     <span className='mx-1'>1:05</span>
-                    <div className="m-2 bg-[#252525] rounded-full" style={{height: '4px', width: '58%'}}>
+                    <div id="play" 
+                         onMouseDown={() => handlePlayUp(true)} onMouseMove={handlePlayMove} onMouseLeave={() =>setColorPlay("#ffffff")} onMouseUp={() => handlePlayUp(false)}
+                         className="m-2 group bg-[#252525] rounded-full" style={{height: '4px', width: '58%', backgroundImage: 'linear-gradient(to right, '+ colorPlay +' '+ gradientPlay + '%, #252525 '+ gradientPlay + '%)'}}>
+                        <div id='myPlay' className="p-1 invisible group-hover:visible bg-[#ffffff] rounded-full" style={{marginTop: "-1%", height: '12px', width: '12px'}}></div>
                     </div>
                     <span className='mx-1'>3:05</span>
                 </div>
@@ -39,9 +110,12 @@ export default function Footer(props) {
                 <Microphone size={16} className="mx-2" style={{marginTop: '6%'}} />
                 <ListTop size={16} className="mx-2 mt-8" style={{marginTop: '6%'}} />
                 <Sound size={16} className="mx-2" style={{marginTop: '6%'}} />
-                <div className="flex mx-2 group w-[100px]" style={{marginTop: '6%'}} >
+                <div className="flex mx-2 w-[100px]" style={{marginTop: '6%'}} >
                     <VolumeNotice size={16} />
-                    <div className="mx-2 bg-[#252525] rounded-full" style={{height: '4px', width: '150px', marginTop: '5%'}}>
+                    <div id="volume" 
+                         onMouseDown={() => handleVolumeUp(true)} onMouseMove={handleVolumeMove} onMouseLeave={() =>setColor("#ffffff")} onMouseUp={() => handleVolumeUp(false)}
+                         className="group p-0 mx-2 rounded-full" style={{height: '4px', width: '150px', marginTop: '5%', backgroundImage: 'linear-gradient(to right, '+ color +' '+ gradient + '%, #252525 '+ gradient + '%)'}}>
+                        <div id='myVolume' className="p-1 invisible group-hover:visible bg-[#ffffff] rounded-full" style={{marginTop: "-5%", height: '12px', width: '12px'}}></div>
                     </div>
                 </div>
                 <ClickToFold size={16} className="mx-2" style={{marginTop: '6%'}} />
