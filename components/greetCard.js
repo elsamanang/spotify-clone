0d295ce2b1f1ae9db0bdaa5705c06fb932c865ce
elsa.m <manangelsa07@gmail.com>
2023-08-09 14:10:14 +0200

    adding links

--- components/greetCard.js
+++ components/greetCard.js
@@ -9,10 +9,10 @@ export default function GreetCard(props) {
 
     return(
         <div className='group flex justify-between m-3 w-[300px] h-[80px] rounded bg-[#353535]/50 hover:bg-[#3a3a3a]'>
-            <div className={'flex'}>
+            <a href="/playlist"><div className={'flex'}>
                 <Image className='w-[80px] h-[80px] rounded' src={props.img} alt="" height={80} width={80} />
                 <p className='mx-4 self-center text-[16px] font-semibold'>{props.titre}</p>
-            </div>
+            </div></a>
             <span className='relative invisible group-hover:visible shadow-lg mt-[15px] mr-[15px] pt-[10px] w-[50px] h-[50px] hover:w-[52px] hover:h-[52px] bg-[#1ed760] text-center rounded-full'>
                 <PlayOne theme={'filled'} fill={'#000000'} onClick={handlePlay} size={28} />
             </span>
